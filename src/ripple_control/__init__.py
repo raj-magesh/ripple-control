afaa7c76e095a86d65e341befc56a5b1c745f99a
Raj Magesh Gauthaman <rmg166@pitt.edu>
2026-08-30 09:56:46 -0400

    add USE_TCP default to public API

--- src/ripple_control/__init__.py
+++ src/ripple_control/__init__.py
@@ -1,15 +1,17 @@
 __all__ = (
+    "USE_TCP",
     "check_xipppy_connection",
-    "send_trigger",
     "send_pulse",
+    "send_trigger",
     "start_recording",
-    "stop_recording",
+    "stop_recording"
 )
 
 from ._utilities import (
+    USE_TCP,
     check_xipppy_connection,
-    send_trigger,
     send_pulse,
+    send_trigger,
     start_recording,
     stop_recording,
 )
